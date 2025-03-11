4007ff41da6ebf61af6e58c5ec3e050d1a3b230d
Bafi Hoang <200473198+bafi-hng@users.noreply.github.com>
2025-03-11 12:40:24 +0100

    Create "new size" function functionality
    Button deletes old grid and creates new sized grid. Still some bugs.

--- script.js
+++ script.js
@@ -1,26 +1,50 @@
+const drawingSize = 960;
 const container = document.querySelector(".container");
+
 let isDrawing = false;
 
-for (let i = 0; i < 255; i++) {
-    const square = document.createElement("div");
-    square.setAttribute("class", `square ${i}`)
-    container.appendChild(square);
+// initial grid
+createGrid();
 
+function createGrid(gridSize = 16) {
+    const grid = document.createElement("div");
+    const squareSize = drawingSize / gridSize;
 
-
-    square.addEventListener("mousedown", () => {
-        event.preventDefault(); 
-        isDrawing = true;
-        square.style.backgroundColor = "black";
-    });
-
-    square.addEventListener("mousemove", () => {
-        if (isDrawing) {
+    for (let i = 0; i < gridSize**2; i++) {
+        const square = document.createElement("div");
+        square.setAttribute("class", `square ${i}`);
+        square.setAttribute("style", `width: ${squareSize}px; height: ${squareSize}px;`);
+        container.appendChild(square);
+    
+        square.addEventListener("mousedown", (event) => {
+            event.preventDefault(); 
+            isDrawing = true;
             square.style.backgroundColor = "black";
-        }
-    });
-
-    square.addEventListener("mouseup", () => {
-        isDrawing = false;
-    })
+        });
+    
+        square.addEventListener("mousemove", () => {
+            if (isDrawing) {
+                square.style.backgroundColor = "black";
+            }
+        });
+    
+        square.addEventListener("mouseup", () => {
+            isDrawing = false;
+        })
+    }
 }
+
+function deleteGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+}
+
+const sizeButton = document.querySelector("button");
+sizeButton.addEventListener("click", () => {
+    let sizeEntered = parseInt(prompt("Enter grid size n"));
+    let newGridSize = sizeEntered > 100 ? 100 : sizeEntered;
+
+    deleteGrid();
+    createGrid(newGridSize);
+});
