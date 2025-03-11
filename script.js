24908fc02454fbfb900a310c8e9bc81f5194cefe
Bafi Hoang <200473198+bafi-hng@users.noreply.github.com>
2025-03-11 11:41:10 +0100

    Add basic drawing ability by dragging mouse

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 const container = document.querySelector(".container");
+let isDrawing = false;
 
 for (let i = 0; i < 255; i++) {
     const square = document.createElement("div");
     square.setAttribute("class", `square ${i}`)
     container.appendChild(square);
+
+
+
+    square.addEventListener("mousedown", () => {
+        event.preventDefault(); 
+        isDrawing = true;
+        square.style.backgroundColor = "black";
+    });
+
+    square.addEventListener("mousemove", () => {
+        if (isDrawing) {
+            square.style.backgroundColor = "black";
+        }
+    });
+
+    square.addEventListener("mouseup", () => {
+        isDrawing = false;
+    })
 }
